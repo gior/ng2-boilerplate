ba0f89fd3f07e92fbcac3c9dec7818b1139ecca8
Giorgio Sidari <giorgio@sidari.it>
2016-07-05 17:53:55 +0200

    Continuous build task

--- app/js/app.component.js
+++ app/js/app.component.js
@@ -1,10 +1,10 @@
-(function(app) {
-  app.AppComponent =
-    ng.core.Component({
-      selector: 'my-app',
-      template: '<h1>An Angular 2 App</h1>'
-    })
-    .Class({
-      constructor: function() {}
-    });
+'use strict';
+
+(function (app) {
+  app.AppComponent = ng.core.Component({
+    selector: 'my-app',
+    template: '<h1>A cool Angular 2 App</h1>'
+  }).Class({
+    constructor: function constructor() {}
+  });
 })(window.app || (window.app = {}));
